f704ee66d8f85a5f926e7ac72e2ed790b60889c2
zira-el <niltonmenezes@live.com>
2021-01-08 20:04:16 -0300

    feat: adicionado o script para calcular

---
+++ index.js
@@ -0,0 +1,13 @@
+function calcular() {
+var alturaElement = document.getElementById("altura");
+var altura = alturaElement.value
+
+var pesoElement = document.getElementById("peso");
+var peso = pesoElement.value
+
+var imc = peso/(altura*altura);
+
+document.getElementById('imc').innerHTML = imc;
+}
+
+
