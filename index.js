cb7ee8160408020f27ac346013b2f65de26c3efc
zira-el <niltonmenezes@live.com>
2021-01-11 14:45:06 -0300

    feat: Classificações do IMC

--- index.js
+++ index.js
@@ -7,7 +7,27 @@ var peso = pesoElement.value
 
 var imc = peso/(altura*altura);
 
-document.getElementById('imc').innerHTML = Math.round(imc);
+document.getElementById('imc').innerHTML = imc.toFixed(1);
+
+if(imc<18.5) {
+    document.getElementById ('Classificação').innerHTML = "abaixo do peso";
+}
+else if (imc >= 18.5 && imc <= 24.9) {
+    document.getElementById ('Classificação').innerHTML = "Peso normal";
+}
+else if (imc >= 25 && imc <= 29.9) {
+    document.getElementById ('Classificação').innerHTML = "Sobrepeso";
+}
+else if (imc >= 30 && imc <= 34.9) {
+    document.getElementById ('Classificação').innerHTML = "Obesidade (Grau I)";
+}
+else if (imc >= 35 && imc <= 39.9) {
+    document.getElementById ('Classificação').innerHTML = "Obesidade (Grau II)";
+}
+else if (imc >= 40) {
+    document.getElementById ('Classificação').innerHTML = "Obesidade (Grau III)";
+}
+
 }
 
 
